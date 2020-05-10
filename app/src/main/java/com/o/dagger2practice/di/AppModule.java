c62c6452698a7cc9ba88505f0ca3ff459a37d8b6
Vansh Arora <vansha10@gmail.com>
2020-05-10 20:30:12 +0530

    Making app component Singleton

--- app/src/main/java/com/o/dagger2practice/di/AppModule.java
+++ app/src/main/java/com/o/dagger2practice/di/AppModule.java
@@ -10,24 +10,29 @@ import com.bumptech.glide.RequestManager;
 import com.bumptech.glide.request.RequestOptions;
 import com.o.dagger2practice.R;
 
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 
 @Module
 public class AppModule {
 
+    @Singleton
     @Provides
     static RequestOptions provideRequestOption() {
         return RequestOptions.placeholderOf(R.drawable.ic_status_check)
                 .error(R.drawable.ic_launcher_foreground);
     }
 
+    @Singleton
     @Provides
     static RequestManager provideGlideInstance(Application application, RequestOptions requestOptions) {
         return Glide.with(application)
                 .setDefaultRequestOptions(requestOptions);
     }
 
+    @Singleton
     @Provides
     static Drawable provideAppDrawable(Application application) {
         return ContextCompat.getDrawable(application, R.drawable.ic_profile);
