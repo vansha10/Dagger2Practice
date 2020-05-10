e7ecddb60f2ea7ea4c98372e1ba703e7c423fe02
Vansh Arora <vansha10@gmail.com>
2020-05-10 17:39:39 +0530

    First commit

---
+++ app/src/main/java/com/o/dagger2practice/BaseApplication.java
@@ -0,0 +1,11 @@
+package com.o.dagger2practice;
+
+import dagger.android.AndroidInjector;
+import dagger.android.support.DaggerApplication;
+
+public class BaseApplication extends DaggerApplication {
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return null;
+    }
+}
