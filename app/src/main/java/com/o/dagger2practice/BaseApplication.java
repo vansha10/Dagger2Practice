0b55c154f0b24cb9de8545a26176986c463f8e44
Vansh Arora <vansha10@gmail.com>
2020-05-10 17:54:10 +0530

    Setup base application

--- app/src/main/java/com/o/dagger2practice/BaseApplication.java
+++ app/src/main/java/com/o/dagger2practice/BaseApplication.java
@@ -1,11 +1,13 @@
 package com.o.dagger2practice;
 
+import com.o.dagger2practice.di.DaggerAppComponent;
+
 import dagger.android.AndroidInjector;
 import dagger.android.support.DaggerApplication;
 
 public class BaseApplication extends DaggerApplication {
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
-        return null;
+        return DaggerAppComponent.builder().application(this).build();
     }
 }
---
+++ app/src/main/java/com/o/dagger2practice/di/AppComponent.java
@@ -0,0 +1,23 @@
+package com.o.dagger2practice.di;
+
+import android.app.Application;
+
+import com.o.dagger2practice.BaseApplication;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Component(modules = {AndroidSupportInjectionModule.class})
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        Builder application(Application application);
+
+        AppComponent build();
+    }
+}
