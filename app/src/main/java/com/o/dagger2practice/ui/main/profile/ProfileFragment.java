66f1b7f6d41a4e6af550d668031dacfc354d0fad
Vansh Arora <vansha10@gmail.com>
2020-05-18 00:07:53 +0530

    Setting ui in profile fragment

--- app/src/main/java/com/o/dagger2practice/ui/main/profile/ProfileFragment.java
+++ app/src/main/java/com/o/dagger2practice/ui/main/profile/ProfileFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,7 @@ import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.o.dagger2practice.R;
+import com.o.dagger2practice.model.User;
 import com.o.dagger2practice.viewmodels.ViewModelProviderFactory;
 
 import javax.inject.Inject;
@@ -22,6 +24,8 @@ public class ProfileFragment extends DaggerFragment {
     private static final String TAG = "ProfileFragment";
 
     private ProfileViewModel viewModel;
+
+    private TextView email, username, website;
 
     @Inject
     ViewModelProviderFactory providerFactory;
@@ -36,6 +40,39 @@ public class ProfileFragment extends DaggerFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
+        email = view.findViewById(R.id.email);
+        username = view.findViewById(R.id.username);
+        website = view.findViewById(R.id.website);
+
         viewModel = ViewModelProviders.of(this, providerFactory).get(ProfileViewModel.class);
+        subscribeObservers();
+    }
+
+    private void subscribeObservers() {
+        viewModel.getAuthenticatedUser().removeObservers(getViewLifecycleOwner());
+        viewModel.getAuthenticatedUser().observe(getViewLifecycleOwner(), userAuthResource -> {
+            if (userAuthResource != null) {
+                switch (userAuthResource.status) {
+                    case AUTHENTICATED:
+                        setUserDetails(userAuthResource.data);
+                        break;
+                    case ERROR:
+                        setErrorDetails(userAuthResource.message);
+                        break;
+                }
+            }
+        });
+    }
+
+    private void setErrorDetails(String message) {
+        email.setText(message);
+        username.setText("error");
+        website.setText("");
+    }
+
+    private void setUserDetails(User data) {
+        email.setText(data.getEmail());
+        username.setText(data.getUsername());
+        website.setText(data.getWebsite());
     }
 }
--- app/src/main/java/com/o/dagger2practice/ui/main/profile/ProfileViewModel.java
+++ app/src/main/java/com/o/dagger2practice/ui/main/profile/ProfileViewModel.java
@@ -2,7 +2,12 @@ package com.o.dagger2practice.ui.main.profile;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.o.dagger2practice.SessionManager;
+import com.o.dagger2practice.model.User;
+import com.o.dagger2practice.ui.auth.AuthResource;
 
 import javax.inject.Inject;
 
@@ -10,8 +15,15 @@ public class ProfileViewModel extends ViewModel {
     
     private static final String TAG = "ProfileViewModel";
 
+    private SessionManager sessionManager;
+
     @Inject
-    public ProfileViewModel() {
+    public ProfileViewModel(SessionManager sessionManager) {
         Log.d(TAG, "ProfileViewModel: viewmodel is working...");
+        this.sessionManager = sessionManager;
+    }
+
+    public LiveData<AuthResource<User>> getAuthenticatedUser() {
+        return sessionManager.getAuthUser();
     }
 }
