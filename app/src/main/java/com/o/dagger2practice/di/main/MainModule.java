16b5676bc713634079d0f397301b4bea4099dca8
Vansh Arora <vansha10@gmail.com>
2020-05-18 00:21:24 +0530

    Added posts api and main module

---
+++ app/src/main/java/com/o/dagger2practice/di/main/MainModule.java
@@ -0,0 +1,16 @@
+package com.o.dagger2practice.di.main;
+
+import com.o.dagger2practice.network.main.MainApi;
+
+import dagger.Module;
+import dagger.Provides;
+import retrofit2.Retrofit;
+
+@Module
+public class MainModule {
+
+    @Provides
+    static MainApi provideMainApi(Retrofit retrofit) {
+        return retrofit.create(MainApi.class);
+    }
+}
---
+++ app/src/main/java/com/o/dagger2practice/network/main/MainApi.java
@@ -0,0 +1,15 @@
+package com.o.dagger2practice.network.main;
+
+import com.o.dagger2practice.model.Post;
+
+import java.util.List;
+
+import io.reactivex.Flowable;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface MainApi {
+
+    @GET("posts")
+    Flowable<List<Post>> getPostsFromUser(@Query("id") int id);
+}
