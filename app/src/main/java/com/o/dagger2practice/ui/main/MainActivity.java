b664bd023303dc876fbf69f01fa913188c0476d9
Vansh Arora <vansha10@gmail.com>
2020-05-24 22:59:02 +0530

    Added nav graph and nav controller

--- app/src/main/java/com/o/dagger2practice/ui/main/MainActivity.java
+++ app/src/main/java/com/o/dagger2practice/ui/main/MainActivity.java
@@ -1,36 +1,51 @@
 package com.o.dagger2practice.ui.main;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.navigation.NavigationView;
 import com.o.dagger2practice.BaseActivity;
 import com.o.dagger2practice.R;
 import com.o.dagger2practice.ui.main.posts.PostsFragment;
 import com.o.dagger2practice.ui.main.profile.ProfileFragment;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "MainActivity";
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        testFragment();
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        init();
     }
 
-    private void testFragment() {
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.main_container, new PostsFragment())
-                .commit();
+    private void init() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -47,5 +62,20 @@ public class MainActivity extends BaseActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_profile:
+                Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.nav_profile);
+                break;
+            case R.id.nav_posts:
+                Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.nav_posts);
+                break;
+        }
+        item.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
