e95348af36fcc708b258d1eb0151dafb3258e1d8
Vansh Arora <vansha10@gmail.com>
2020-05-28 22:40:03 +0530

    Added scopes - auth scope and main scope

--- app/src/main/java/com/o/dagger2practice/di/ActivityBuildersModule.java
+++ app/src/main/java/com/o/dagger2practice/di/ActivityBuildersModule.java
@@ -1,9 +1,11 @@
 package com.o.dagger2practice.di;
 
 import com.o.dagger2practice.di.auth.AuthModule;
+import com.o.dagger2practice.di.auth.AuthScope;
 import com.o.dagger2practice.di.auth.AuthViewModelsModule;
 import com.o.dagger2practice.di.main.MainFragmentBuildersModule;
 import com.o.dagger2practice.di.main.MainModule;
+import com.o.dagger2practice.di.main.MainScope;
 import com.o.dagger2practice.di.main.MainViewModelsModule;
 import com.o.dagger2practice.ui.auth.AuthActivity;
 import com.o.dagger2practice.ui.main.MainActivity;
@@ -14,9 +16,11 @@ import dagger.android.ContributesAndroidInjector;
 @Module
 public abstract class ActivityBuildersModule {
 
+    @AuthScope
     @ContributesAndroidInjector(modules = {AuthViewModelsModule.class, AuthModule.class})
     abstract AuthActivity contributeAuthActivity();
 
+    @MainScope
     @ContributesAndroidInjector(modules = {
             MainFragmentBuildersModule.class,
             MainViewModelsModule.class,
